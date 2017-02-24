de6320b6bed51a43b79bdfe0a91b602514b3dad9
Ugyen Jigten <ujigten@gmail.com>
2017-02-24 20:32:57 +0700

    Low power sleep and interrupt using FSR

--- main/main.ino
+++ main/main.ino
@@ -1,19 +1,48 @@
 #include <LowPower.h>
 
+int pin2 = 2;
 int ledPin   = 13;
 
-void setup()
-{   pinMode(ledPin, OUTPUT);
+void pin2interrupt(void)
+{
+  // Function called when awoken from sleep
+  // Detach interrupt to stop it from continuosly firing when in normal mode
 }
 
-void loop()
-{   for (int i=0; i<3; i++)     // LED will blink 3 times
-    {  delay(2000);
-       digitalWrite(ledPin, HIGH);
-       delay(2000);
-       digitalWrite(ledPin, LOW);
-    } 
+void enterSleep(void)
+{
+  attachInterrupt(0, pin2interrupt, HIGH);
+  Serial.println("Sleeping");
+  delay(100);
+  LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+  Serial.println("Awake!");
+}
 
-    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+
+void setup()
+{ 
+  Serial.begin(9600);
+  pinMode(pin2, INPUT);  
+  pinMode(ledPin, OUTPUT);
+}
+
+int seconds = 0;
+void loop()
+{   
+  delay(1000);
+  seconds++;
+  Serial.println("Awake in the loop!");
+  Serial.println(seconds);
+    if(digitalRead(pin2) == LOW && seconds == 3)
+    {
+      seconds = 0;
+      Serial.println("No child detected, so going to sleep!");
+      delay(200);
+      enterSleep();
+    }
+    else if(digitalRead(pin2) == HIGH && seconds == 3)
+    {
+      seconds = 0;
+    }
 }
 

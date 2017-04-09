da53529d1f96653893c5bc66eb3877444164f2ce
Ugyen Jigten <ujigten@gmail.com>
2017-04-09 23:56:04 +0700

    Wait 5 seconds for child or sleep

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,14 @@
 #include <LowPower.h>
+#include <SoftwareSerial.h>
+ 
+//SIM800 TX is connected to Arduino D8
+#define SIM800_TX_PIN 8
+ 
+//SIM800 RX is connected to Arduino D7
+#define SIM800_RX_PIN 7
+ 
+//Create software serial object to communicate with SIM800
+SoftwareSerial serialSIM800(SIM800_TX_PIN,SIM800_RX_PIN);
 
 int pin2 = 2;
 int ledPin   = 13;
@@ -22,27 +32,33 @@ void enterSleep(void)
 void setup()
 { 
   Serial.begin(9600);
+   
+  //Being serial communication with Arduino and SIM800
+  serialSIM800.begin(9600);
+  delay(1000);
+  
   pinMode(pin2, INPUT);  
   pinMode(ledPin, OUTPUT);
+  Serial.println("Setup Complete!");
 }
 
-int seconds = 0;
+int iterations = 0;
 void loop()
 {   
   delay(1000);
-  seconds++;
+  iterations ++;
   Serial.println("Awake in the loop!");
-  Serial.println(seconds);
-    if(digitalRead(pin2) == LOW && seconds == 3)
+  Serial.println(iterations);
+    if(digitalRead(pin2) == LOW && iterations == 5)
     {
-      seconds = 0;
+      iterations = 0;
       Serial.println("No child detected, so going to sleep!");
       delay(200);
       enterSleep();
     }
-    else if(digitalRead(pin2) == HIGH && seconds == 3)
+    else if(digitalRead(pin2) == HIGH && iterations == 10)
     {
-      seconds = 0;
+      iterations = 0;
     }
 }
 
